f70d332ecd77074e5cbb5bbfd9dd1ba641131428
Tanush <23951a66m9@iare.ac.in>
2026-08-02 01:30:58 +0530

    feat: PostgreSQL connection setup + env template

--- src/config.py
+++ src/config.py
@@ -13,3 +13,11 @@ PROCESSED_DATA_DIR = PROJECT_ROOT / "data" / "processed"
 
 KAGGLE_DATASET_CAREER = "kushagrajain19/f1-drivers-dataset-1950-2026"
 KAGGLE_DATASET_RACES = "rohanrao/formula-1-world-championship-1950-2020"
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
