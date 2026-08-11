f058d985bee57d22f2421f0358237fb6fbc17c7c
Tanush <23951a66m9@iare.ac.in>
2026-08-11 23:22:12 +0530

    feat: Day 7 - Tableau BI view, KPI definitions, .hyper extract export pipeline

--- sql/views/vw_tableau_main.sql
+++ sql/views/vw_tableau_main.sql
@@ -22,11 +22,10 @@ SELECT
         WHEN s.status IN ('Accident', 'Collision', 'Spun off', 'Collision damage') THEN 'Accident'
         ELSE 'Mechanical/Other'
     END AS status_category,
-    (f.grid - f.position_order) AS grid_delta
+    CASE WHEN f.grid > 0 THEN (f.grid - f.position_order) ELSE NULL END AS grid_delta
 FROM fact_race_results f
 JOIN dim_driver d ON f.driver_key = d.driver_key
 JOIN dim_constructor c ON f.constructor_key = c.constructor_key
 JOIN dim_race r ON f.race_key = r.race_key
 JOIN dim_circuit ci ON r.circuit_key = ci.circuit_key
 JOIN dim_status s ON f.status_key = s.status_key
-WHERE f.grid > 0 OR f.grid IS NULL;
